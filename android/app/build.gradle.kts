8def93cc244af6e9cb87113b34d5627334857e88
glog2105 <154365711+glog2105@users.noreply.github.com>
2025-04-26 15:30:55 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,56 +1,53 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.mobtox"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-packaging {
-    jniLibs {
-        pickFirsts += setOf(
-            "lib/armeabi-v7a/libtox.so",
-            "lib/arm64-v8a/libtox.so"
-        )
-    }
-    resources.excludes += setOf(
-        "META-INF/AL2.0",
-        "META-INF/LGPL2.1"
-    )
-}
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
+    namespace = "com.mobtox"
+    compileSdk = 34
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mobtox"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        applicationId = "com.mobtox"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        ndk {
+            abiFilters.addAll(setOf("armeabi-v7a", "arm64-v8a", "x86_64"))
+        }
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++17"
+                arguments += listOf(
+                    "-DANDROID_STL=c++_shared",
+                    "-DCMAKE_VERBOSE_MAKEFILE=ON"
+                )
+            }
+        }
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                file("proguard-rules.pro")
+            )
         }
+    }
+
+    packaging {
+        jniLibs.useLegacyPackaging = true
+        resources.excludes += setOf(
+            "**/libtox.so",
+            "META-INF/**"
+        )
     }
 }
 
-flutter {
-    source = "../.."
+dependencies {
+    implementation("net.zetetic:android-database-sqlcipher:4.5.3")
+    implementation("androidx.security:security-crypto-ktx:1.1.0-alpha06")
 }
